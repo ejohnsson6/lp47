f57cfbf5dff87dac0391cc2ac0ec79cd6970c29a
Vidar Magnusson <git@vidarmagnusson.com>
2020-10-03 16:28:22 +0200

    Began adding enemies

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,34 @@
+﻿
+using System;
+
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField]
+    private int maxHealth = 1;
+    private int health;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = maxHealth;
+        KillableFromAbove script = gameObject.AddComponent<KillableFromAbove>();
+        script.RegisterOnDamageTaken(TakeDamage);
+    }
+
+    public void TakeDamage(int amount)
+    {
+        TakeDamage(amount, null);
+    }
+
+    public void TakeDamage(int amount, Action onDeathCallback)
+    {
+        health -= amount;
+        if (health <= 0)
+        {
+            onDeathCallback?.Invoke();
+            Destroy(gameObject, 0.1f);
+        }
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -15,18 +14,29 @@ public class Player : MonoBehaviour
     private Vector2 startPosition;
     private Quaternion startRotation;
 
+    private Vector2 leftRaycastOrigin;
+    private Vector2 rightRaycastOrigin;
+
+    private Vector2 rayCastVector;
+
+    private BoxCollider2D boxCollider;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidbody = GetComponent<Rigidbody2D>();
+        boxCollider = GetComponent<BoxCollider2D>();
         startPosition = transform.position;
         startRotation = transform.rotation;
+        rayCastVector = Vector2.up * 0.001f;
     }
 
     // Update is called once per frame
     void Update()
     {
+        leftRaycastOrigin = new Vector2(transform.position.x - (boxCollider.size.x / 2) * 0.99f, transform.position.y - (boxCollider.size.y / 2) * 0.99f - rayCastVector.y);
+        rightRaycastOrigin = new Vector2(transform.position.x + (boxCollider.size.x / 2) * 0.99f, transform.position.y + (boxCollider.size.y / 2) * 0.99f - rayCastVector.y);
+
         if (transform.position.y <= minAllowedY)
         {
             Reset();
@@ -35,7 +45,8 @@ public class Player : MonoBehaviour
         if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
         {
             transform.Translate(Vector3.right * Time.deltaTime * speed);
-        } else if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
         {
             transform.Translate(Vector3.left * Time.deltaTime * speed);
         }
@@ -51,5 +62,24 @@ public class Player : MonoBehaviour
         transform.position = startPosition;
         transform.rotation = startRotation;
         rigidbody.velocity = Vector2.zero;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag.Contains("enemy_"))
+        {
+            // We hit an enemy, if we hit it from above, kill it
+            // Otherwise we should take damage or something
+            RaycastHit2D left = Physics2D.Raycast(leftRaycastOrigin, rayCastVector);
+            RaycastHit2D right = Physics2D.Raycast(rightRaycastOrigin, rayCastVector);
+
+
+
+            if (left.collider.gameObject == collision.gameObject || right.collider.gameObject == collision.gameObject)
+            {
+                // Kill it!
+            }
+        }
+
     }
 }
