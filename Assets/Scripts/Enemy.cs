634c1bfb2c004917b8c4dda528f7088e4c9e773c
Swexbe <erik@erikjohnsson.se>
2020-10-03 20:24:45 +0200

    Added ai to enemies

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -9,12 +9,25 @@ public class Enemy : MonoBehaviour
     private int maxHealth = 1;
     private int health;
 
+    [SerializeField] private float timeToSwapDirection = 2f;
+
+    [SerializeField] private float speed = 2f;
+
+    [SerializeField] private float startTime = 0f;
+    
+    private bool right;
+
+    private float count;
+
     // Start is called before the first frame update
     void Start()
     {
         health = maxHealth;
         KillableFromAbove script = gameObject.AddComponent<KillableFromAbove>();
         script.RegisterOnDamageTaken(TakeDamage);
+
+        right = true;
+        count = startTime;
     }
 
     public void TakeDamage(int amount)
@@ -29,6 +42,25 @@ public class Enemy : MonoBehaviour
         {
             onDeathCallback?.Invoke();
             Destroy(gameObject, 0.0f);
+        }
+    }
+
+    private void Update()
+    {
+        count += Time.deltaTime;
+        if (right)
+        {
+            transform.Translate(Vector3.right * (Time.deltaTime * speed));
+        }
+        else
+        {
+            transform.Translate(Vector3.left * (Time.deltaTime * speed));
+        }
+
+        if (count > timeToSwapDirection)
+        {
+            right = !right;
+            count = 0;
         }
     }
 }
